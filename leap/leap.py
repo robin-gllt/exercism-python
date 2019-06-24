280d44b0cb258ddb8a6fa359d182166ab5c93697
rguillat <34415960+rguillat@users.noreply.github.com>
2019-06-24 14:16:44 +0200

    add Rational Numbers exo

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,15 @@
 def leap_year(year):
-    pass
+
+    if year % 4 == 0:
+        if year % 400 ==0:
+            return True
+        elif year % 100 == 0:
+            return False
+        return True
+    else:
+        return False
+
+# if __name__ == '__main__':
+#     print(leap_year(2000))
+
+
